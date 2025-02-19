2f863bc21be9043666f64f08c5e40b9424719689
AnanthuSivakumar <ananthusivakumar0@gmail.com>
2025-02-18 23:04:49 -0500

    Updated files with new changes

--- src/Components/Rowposter/Rowposter.jsx
+++ src/Components/Rowposter/Rowposter.jsx
@@ -1,55 +1,64 @@
-import { useEffect, useState } from 'react';
-import YouTube from 'react-youtube';
-import axios from '../../Axios';
-import {image_url,API_Key}from "../../Constants/Constants"
-import "./Rowposter.css"
+import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
+import axios from "../../Axios";
+import { image_url, API_Key } from "../../Constants/Constants";
+import "./Rowposter.css";
 
 function Rowposter(props) {
-    const [action,setAction]=useState([])
-    const [urlId,setUrlId]=useState("")
-  useEffect(()=>{
-    axios.get(props.url).then((response)=>{
-      setAction(response.data.results)
-      console.log(response.data.results[1])
-    })
-  },)
+  const [action, setAction] = useState([]);
+  const [urlId, setUrlId] = useState("");
+  useEffect(() => {
+    axios.get(props.url).then((response) => {
+      setAction(response.data.results);
+      console.log(response.data.results[1]);
+    });
+  });
   const opts = {
-    height: '260',
-    width: '100%',
-      autoplay: 1,}
- 
-  const handleMovie=(id)=>{
-    console.log(id)
-axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_Key}&language=en-US`).then((response)=>{
-if(response.data.results.lngth!==0){
-  setUrlId(response.data.results[0])
-}
-})
+    height: "260",
+    width: "100%",
+    autoplay: 1,
+  };
 
-  }
-  const handleCloseVideo = ()=>{
-   setUrlId("")
-  }
+  const handleMovie = (id) => {
+    console.log(id);
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_Key}&language=en-US`
+      )
+      .then((response) => {
+        if (response.data.results.lngth !== 0) {
+          setUrlId(response.data.results[0]);
+        }
+      });
+  };
+  const handleCloseVideo = () => {
+    setUrlId("");
+  };
   return (
-    <div className='row'>
-        <h2>{props.title}</h2>
-        <div className="posterss">
-          {
-            action.map((obj)=>
-              <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? "smallPoster" : 'poster'} src={`${image_url+obj.backdrop_path}`}alt="" />
-            )
-          }
-           
-          
-        </div>
-        {  urlId && <button style={{float:'right',marginTopTop:"230px"}} onClick={handleCloseVideo}>Close Video</button>}
-       { urlId && <YouTube videoId={urlId.key} opts={opts}  />}
-      
-       
-
-     
+    <div className="row">
+      <h2>{props.title}</h2>
+      <div className="posterss"> 
+        {action.map((obj) => (
+          <img
+            onClick={() => handleMovie(obj.id)}
+            className={props.isSmall ? "smallPoster" : "poster"}
+            src={`${image_url + obj.backdrop_path}`}
+            alt=""
+          />
+        ))}
+      </div>
+      {urlId && (
+        <button
+          style={{ float: "right", marginTopTop: "230px" }}
+          onClick={handleCloseVideo}
+        >
+          Close Video
+        </button>
+      )}
+      {urlId && <YouTube videoId={urlId.key} opts={opts} />}
+      {}
     </div>
-  )
+  );
 }
 
 export default Rowposter;
